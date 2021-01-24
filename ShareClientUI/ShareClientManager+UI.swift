7d88841cfb14624bdbef2233ddd334d27f7976a1
Darin Krauss <darin@tidepool.org>
2021-01-24 12:32:37 -0800

    LOOP-2955 Allow onboarding to utilize other plugins
    - https://tidepool.atlassian.net/browse/LOOP-2955
    - Update to latest CGM manager requirements for onboarding

--- ShareClientUI/ShareClientManager+UI.swift
+++ ShareClientUI/ShareClientManager+UI.swift
@@ -12,25 +12,25 @@ import HealthKit
 import ShareClient
 
 extension ShareClientManager: CGMManagerUI {
-    public static func setupViewController(glucoseTintColor: Color, guidanceColors: GuidanceColors) -> (UIViewController & CGMManagerSetupViewController & CompletionNotifying)? {
-        return ShareClientSetupViewController()
+    public static func setupViewController(colorPalette: LoopUIColorPalette) -> UIResult<UIViewController & CGMManagerCreateNotifying & CGMManagerOnboardNotifying & CompletionNotifying, CGMManagerUI, Error> {
+        return .userInteractionRequired(ShareClientSetupViewController())
     }
 
-    public func settingsViewController(for glucoseUnit: HKUnit, glucoseTintColor: Color, guidanceColors: GuidanceColors) -> (UIViewController & CompletionNotifying & PreferredGlucoseUnitObserver) {
+    public func settingsViewController(for glucoseUnit: HKUnit, colorPalette: LoopUIColorPalette) -> (UIViewController & CGMManagerOnboardNotifying & PreferredGlucoseUnitObserver & CompletionNotifying) {
         let settings = ShareClientSettingsViewController(cgmManager: self, glucoseUnit: glucoseUnit, allowsDeletion: true)
-        let nav = CGMManagerSettingsNavigationController(rootViewController: settings)
+        let nav = CGMManagerSettingsNavigationViewController(rootViewController: settings)
         return nav
     }
 
     public var smallImage: UIImage? {
         return nil
     }
-    
+
     // TODO Placeholder. This functionality will come with LOOP-1311
     public var cgmStatusHighlight: DeviceStatusHighlight? {
         return nil
     }
-    
+
     // TODO Placeholder. This functionality will come with LOOP-1311
     public var cgmLifecycleProgress: DeviceLifecycleProgress? {
         return nil
